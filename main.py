e488a7cc355804d92b256f32b49312c825bcda4a
Andreycher <rugewit@yandex.ru>
2019-02-01 00:48:42 +0300

    в процессе2

--- main.py
+++ main.py
@@ -14,7 +14,6 @@ from PyQt5.QtGui import QPainter, QPixmap
 from PyQt5.QtCore import (QObject, QPointF,
                           QPropertyAnimation, pyqtProperty)
 import sys
-# qwe
 from PyQt5.uic.properties import QtGui
 #import settings
 from PyQt5.QtGui import QTransform
@@ -29,7 +28,7 @@ import shutil
 img = QImage('bg.jpg')
 img_path = 'bg.jpg'
 mode = 1
-#edit = QTextEdit()
+x_tmp , y_tmp = (0,0)
 
 class Label(QLabel):
     def __init__(self):
@@ -41,45 +40,29 @@ class Label(QLabel):
         self.has_img = True
 
     def paintEvent(self, QPaintEvent):
-        global mode
+        global mode,x_tmp , y_tmp
         temp = None
         super().paintEvent(QPaintEvent)
-        try:
-            temp = self.pixmap()
-            print(temp)
-            print(temp.rect())
-            print(temp.size())
-            x0 = (self.width() - temp.width()) / 2
-            y0 = (self.height() - temp.height()) / 2
-            if x0 < 0:
-                x0 = 0
-            if y0 < 0:
-                y0 = 0
-            print(x0, y0)
-        except Exception as e:
-            print(e)
+        temp = self.pixmap()
+        print(temp)
+        print(temp.rect())
+        print(temp.size())
+        x0 = (self.width() - temp.width()) / 2
+        y0 = (self.height() - temp.height()) / 2
+        if x0 < 0:
+            x0 = 0
+        if y0 < 0:
+            y0 = 0
+        print(x0, y0)
         painter = QPainter(self)
         if mode == 2:
             painter.setBrush(QBrush(QColor('white')))
             painter.drawRect(x0,y0,settings.width,settings.height)
+            if mode == 3:
+                painter.setPen(QPen(QColor('green'), 5, Qt.SolidLine, Qt.RoundCap))
+                painter.drawPoint(x_tmp , y_tmp)
+                painter.setPen(QPen(QColor('red'), 1, Qt.SolidLine, Qt.RoundCap))
         if self.has_img:
-            '''
-            painter.setPen(QPen(QColor('green'), 1, Qt.SolidLine, Qt.RoundCap))
-
-           
-            painter.setBrush(QBrush(QColor('green'), Qt.SolidPattern))
-            painter.drawRect(0+x0, 0+y0,
-                 20, 20)
-            painter.setBrush(QBrush(QColor('yellow'), Qt.SolidPattern))
-            painter.drawRect(21+x0, 0+y0,
-                 20, 20)
-            painter.setBrush(QBrush(QColor('blue'), Qt.SolidPattern))
-            painter.drawRect(41+x0, 0+y0,
-                 20, 20)
-            painter.setBrush(QBrush(QColor('brown'), Qt.SolidPattern))
-            painter.drawRect(61+x0, 0+y0,
-                 20, 20)
-                 '''
             width = temp.width()
             height = temp.height()
             painter.setPen(QPen(QColor('red'), 1, Qt.SolidLine, Qt.RoundCap))
@@ -89,18 +72,23 @@ class Label(QLabel):
                 painter.drawLine(x0 , y0 + i * (height // settings.count_y)  ,x0 + width, y0 + i * (height // settings.count_y)  )
 
 
-
+    def mousePressEvent(self, e):
+        global mode
+        super().mousePressEvent(e)
+        x_tmp,y_tmp = e.x(), e.y()
+        if 121<=x_tmp<=968 and 94<=y_tmp<=571 and mode == 2:
+            i = (x_tmp - 121) // (self.pixmap().width()//settings.count_x)
+            y = (y_tmp - 94) // (self.pixmap().height()//settings.count_y)
+            x_tmp, y_tmp = i, y
+            mode = 3
+            self.update()
+        self.update()
 
 def initUI(self):
     uic.loadUi('MainWindow.ui', self)
     self.btn_load_img.clicked.connect(self.load_image)
     self.imageLabel = Label()
-
-    #self.scene = Scene(self.graphicsView.size().width(),self.graphicsView.size().height())
-
-    #self.graphicsView.setScene(self.scene)
     self.frame_copy = self.frame
-
     self.scrollArea.setWidget(self.imageLabel)
     self.imageLabel.setAlignment( Qt.AlignVCenter | Qt.AlignCenter)
     self.btn_confirm.clicked.connect(self.confirm_input)
@@ -108,8 +96,6 @@ def initUI(self):
     self.btn_crop.clicked.connect(self.start_croping)
     self.btn_mode_1.clicked.connect(self.start_mode_1)
     self.btn_mode_2.clicked.connect(self.start_mode_2)
-    #self.setCentralWidget(self.scrollArea)
-    #crop()
 
 
 def crop(path,x1, y1, x2, y2,x,y):
@@ -133,23 +119,14 @@ class MainWnd(QMainWindow):
 
         global img,img_path
         fname = None
-        try:
-            fname,_filter = QFileDialog.getOpenFileName(self, 'Open file',
-                                                'c:\\', "Image files (*.jpg *.png)")
-        except Exception as e:
-            print(123)
+        fname,_filter = QFileDialog.getOpenFileName(self, 'Open file',
+                                            'c:\\', "Image files (*.jpg *.png)")
         if fname != None:
             img = QImage(fname)
             img_path = fname
             settings.width = img.width()
             settings.height = img.height()
-            try:
-                #self.graphicsView.resize(img.width(),img.height())
-                #self.scene.setSceneRect(0, 0, img.width(), img.height())
-                self.imageLabel.setImage(img)
-                #дописать
-            except Exception as e:
-                print(e)
+            self.imageLabel.setImage(img)
         print('by loading',img)
 
     def output_folder(self):
@@ -164,86 +141,42 @@ class MainWnd(QMainWindow):
     def start_mode_2(self):
         global mode
         print('mode2')
-        mode = 2
-        self.imageLabel.update()
-        '''
         try:
-            for i in range(settings.height//2):
-                for j in range(settings.width//2):
-                    qle = QLineEdit()
-                    qle.move(i+50, j+50)
-        except Exception as e:
-            print(e)
-        
-        try:
-            qle = QLineEdit()
-            self.scrollArea.addWidget(qle)
-            qle.move( 50,50)
+            mode = 2
             self.imageLabel.update()
+            #self.frame_copy.setParent(self.imageLabel)
+            self.show_dialog()
+        except Exception as e:
+            print(e)
 
-        except Exception as e:
-            print(e)
-        '''
-        try:
-            self.frame_copy.setParent(self.imageLabel)
-        except Exception as e:
-            print(e)
+    def show_dialog(self):
+        d = QDialog()
+        d.setWindowTitle("Dialog")
+        d.setWindowModality(Qt.ApplicationModal)
+        d.exec_()
 
     def confirm_input(self):
-        try:
-            settings.count_x = int(self.lineEdit_x.text())
-            settings.count_y = int(self.lineEdit_y.text())
-            self.imageLabel.update()
-        except Exception as e:
-            print(e)
+        settings.count_x = int(self.lineEdit_x.text())
+        settings.count_y = int(self.lineEdit_y.text())
+        self.imageLabel.update()
+
 
     def start_croping(self):
         global img,img_path
-        '''
-        try:
-            for the_file in os.listdir(folder):
-                file_path = os.path.join(folder, the_file)
-                try:
-                    if os.path.isfile(file_path):
-                        os.unlink(file_path)
-                        # elif os.path.isdir(file_path): shutil.rmtree(file_path)
-                except Exception as e:
-                    print(e)
-            
-            crop('d://temp//',0, 0,
-                 20, 20, 0, 0)
-            crop('d://temp//', 21, 0,
-                 41, 20, 0, 1)
-            crop('d://temp//', 41, 0,
-                 61, 20, 1, 0)
-            crop('d://temp//', 61, 0,
-                 81, 20, 1, 1)
-            
-        except Exception as e:
-            print(e)
-        '''
-        try:
-            print(settings.width,settings.height)
-            for i in range(settings.count_x):
-                for y in range(settings.count_y):
-                    print('x1:',i*(settings.width//settings.count_x))
-                    #print('deltax:',(settings.width//settings.count_x))
-                    print('y1',y*(settings.height//settings.count_y))
-                    #print('deltay',(settings.height//settings.count_y))
-                    x2 = (i+1)*(settings.width//settings.count_x)
-                    y2 = (y+1)*(settings.height//settings.count_y)
-                    crop(settings.output_folder,i*(settings.width//settings.count_x),y*(settings.height//settings.count_y),
-                         x2,y2,i,y)
-        except Exception as e:
-            print(e)
-
-
+        print(settings.width,settings.height)
+        for i in range(settings.count_x):
+            for y in range(settings.count_y):
+                print('x1:',i*(settings.width//settings.count_x))
+                #print('deltax:',(settings.width//settings.count_x))
+                print('y1',y*(settings.height//settings.count_y))
+                #print('deltay',(settings.height//settings.count_y))
+                x2 = (i+1)*(settings.width//settings.count_x)
+                y2 = (y+1)*(settings.height//settings.count_y)
+                crop(settings.output_folder,i*(settings.width//settings.count_x),y*(settings.height//settings.count_y),
+                     x2,y2,i,y)
 
     def getfile(self):
         pass
-        #self.le.setPixmap(QPixmap(fname))
-
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
